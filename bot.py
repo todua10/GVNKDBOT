306a1936728926f936e346c02e08fa52e9580164
dontneed <dontneedtocallname@gmail.com>
2022-04-10 09:41:10 +0500

    test

--- bot.py
+++ bot.py
@@ -16,34 +16,35 @@ class Bot:
     def __init__(self, user_id):
         self._user_id = user_id
         self.welcome_message_sent = False
-        self.next_command = "talk" # Переменная необходимая, чтобы переключать режим работы бота
+        self.next_task = "talk" # Переменная необходимая, чтобы переключать режим работы бота
         self.current_quesion = 0
 
-    # Принимает команду пользоваетеля и обрабатывает её. В качестве ответа отправляет класс Answer
+    # Вызывает функцию для текущей задачи. Нужно для того, чтобы можно было обращаться к одной функции
     def update(self, text):
         # Передается входное значение в функцию
-        if self.next_command == "talk":
+        if self.next_task == "talk":
             return self.talk(text)
 
-        if self.next_command == "welcome":
+        if self.next_task == "welcome":
             return self.welcome()
 
-        if self.next_command == "test":
+        if self.next_task == "test":
             return self.test(text)
 
-        if self.next_command == "show_meme":
+        if self.next_task == "show_meme":
             return self.show_meme(text)
 
-        if self.next_command == "show_user_stat":
+        if self.next_task == "show_user_stat":
             return self.show_user_stat(text)
 
-        if self.next_command == "show_all_stat":
+        if self.next_task == "show_all_stat":
             return self.show_all_stat(text)
 
-        if self.next_command == "upload_meme":
+        if self.next_task == "upload_meme":
             return self.upload_meme(text)
 
 
+# Здесь бот принимает сообщения и определяет дальнейшую работу
     def talk(self, text):
         if text.lower() in ("привет", "ку", "здарвствуйте", "вернуться в начало", "начало"):
             return self.welcome()
@@ -80,6 +81,7 @@ class Bot:
         else:
             return Answer("Привет еще раз!")
 
+
     def help(self):
         return Answer("Список доступных команд:\n\n"
                       "начать тест\n"
@@ -90,14 +92,14 @@ class Bot:
 
 
     def test(self, text):
-        self.next_command = "test"
+        self.next_task = "test"
 
         if self.current_quesion == 0:
             return Answer("1")
 
 
     def show_meme(self, text):
-        #self.next_command = "show_meme"
+        #self.next_task = "show_meme"
         keyboard = VkKeyboard(inline=True)
         keyboard.add_button("Заебись", color=VkKeyboardColor.POSITIVE)
         keyboard.add_button("Хуево", color=VkKeyboardColor.NEGATIVE)
@@ -105,10 +107,10 @@ class Bot:
 
 
     def show_stat(self, text):
-        #self.next_command = "show_user_stat"
+        #self.next_task = "show_stat"
         return Answer("Функция пока не доступна")
 
 
     def upload_meme(self, text):
-        #self.next_command = "upload_meme"
+        #self.next_task = "upload_meme"
         return Answer("Функция пока не доступна")
